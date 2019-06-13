4851344aa949a992a48bc756fb43bfe9fad71ed3
Danilo Santo <danilohenriqueoliveirasantos@gmail.com>
2019-06-13 20:55:36 -0300

    Associando métodos do controller às ações do usuário

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,19 @@ class NegociacaoController {
 
     adiciona() {
         event.preventDefault();
-        alert("Chamei a ação no Controller");
+
+        let $ = document.querySelector.bind(document);
+        
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+        
+        
+        
     }
 
 }
