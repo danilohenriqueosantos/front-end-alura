00a4cbfdb58b140ddc686993c6f11711c09a6406
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-06-17 14:54:32 -0300

    Construindo armadilhas de leitura

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,13 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
-
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
         // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
@@ -21,7 +18,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-        this._armadilha(this);
         // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
