a5d80c3a30a7588f7a5eea2481f0bd3ad6acc638
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-06-21 12:43:39 -0300

    O problema da vida assíncrona

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -30,9 +30,28 @@ class NegociacaoController {
             }
 
             negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = "Negociações importadas com sucesso!";
 
+            service.obterNegociacoesDaSemanaAnterior((err, negociacoes) => {
+                if (err) {
+                    this._mensagem.texto = err;
+                    return;
+                }
+
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+
+                service.obterNegociacoesDaSemanaRetrasada((err, negociacoes) => {
+                    if (err) {
+                        this._mensagem.texto = err;
+                        return;
+                    }
+
+                    negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = "Negociações importadas com sucesso!";
+
+                });
+            });
         });
+
     }
 
     apaga() {
