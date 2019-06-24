57c6b28c16bbd02f8d897f552c289189a3ca7599
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-06-24 15:43:46 -0300

    Cliclo de vida e DOM virtual

--- react_parte_1/cdc-admin/src/App.js
+++ react_parte_1/cdc-admin/src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
                   {
                     this.state.lista.map((autor) => {
                       return (
-                        <tr>
+                        <tr key={autor.id}>
                           <td>{autor.nome}</td>
                           <td>{autor.email}</td>
                         </tr>
