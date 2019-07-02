d0e203fb867cf06cda6c8b29b2bf2c9d000e6c34
Danilo Santo <danilohenriqueoliveirasantos@gmail.com>
2019-07-01 23:46:45 -0300

    Metódo fetch

--- instalura/src/components/Timeline.jsx
+++ instalura/src/components/Timeline.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import Foto from './Foto';
 
 export default class Timeline extends Component {
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/public/fotos/alots');
+    }
+
     render() {
         return (
             <div className="fotos container">
                 <Foto />
-                {/* <Foto /> */}
+                <Foto />
             </div>
         );
     }
