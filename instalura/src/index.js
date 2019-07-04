0292eea7d82628cab3a9c04ab77c9759332d87a5
Danilo Santo <danilohenriqueoliveirasantos@gmail.com>
2019-07-03 21:01:23 -0300

    1 - Leberando o acesso para timeline pública

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -7,9 +7,13 @@ import App from './App';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import { Router, Route, browserHistory } from 'react-router';
+import { matchPattern } from 'react-router/lib/PatternUtils';
 
 function verificarAutenticacao(nextState, replace) {
-    if (localStorage.getItem('auth-token') === null) {
+    const resultado = matchPattern("/timeline(/:login)", nextState.location.pathname);
+    console.log(resultado);
+    const enderecoPrivadoTimeline = resultado.paramValues[0] === undefined;
+    if (enderecoPrivadoTimeline && localStorage.getItem('auth-token') === null) {
         replace('/?msg=Você precisa estar logado para acessar o endereço');
     }
 }
@@ -19,7 +23,7 @@ ReactDOM.render(
     (
         <Router history={browserHistory}>
             <Route path="/" component={Login} />
-            <Route path="/timeline" component={App} onEnter={verificarAutenticacao} />
+            <Route path="/timeline(/:login)" component={App} onEnter={verificarAutenticacao} />
             <Route path="logout" component={Logout} />
         </Router>
     ),
